f9963c04688191eae481d7fd631e88bfa762e762
Jonathan Villegas <jonathan.e.villegas@gmail.com>
2020-04-13 10:31:22 -0700

    First commit without .gitignore. Added world position component.

---
+++ Source/BuildingEscape/Components/BEWorldPosition.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BEWorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UBEWorldPosition::UBEWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UBEWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+// 	FString Log = TEXT("Some text");
+// 	FString* PtrLog = &Log;
+// 	// ...
+// 	UE_LOG(LogTemp, Warning, TEXT("%s"), *Log);
+	FString OwnerName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Owner's name: %s"), *OwnerName);
+}
+
+
+// Called every frame
+void UBEWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
